bcbed84293f8fe06eec343ec999b023d81f048c6
David Wasilewko <david_was@mac.com>
2022-11-25 10:33:03 +0100

    Added View

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Notifications from 'expo-notifications';
+import { AndroidNotificationPriority } from 'expo-notifications';
+
+import NotificationView from "./src/views/NotificationView";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+    priority: AndroidNotificationPriority.HIGH
+  }),
+});
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <NotificationView/>
     </View>
   );
 }
